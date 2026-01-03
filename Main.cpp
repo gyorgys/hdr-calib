c2ab08d050c885d5896536a75110cf99afd22d8c
gyorgys <gyorgy@outlook.com>
2026-01-02 16:39:38 -0800

    HDR colorspace

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 #include <windows.h>
 #include <d3d11.h>
-#include <dxgi1_2.h>
+#include <dxgi1_4.h>
 #include <d2d1_1.h>
 #include <wrl/client.h>
 
@@ -9,14 +9,16 @@ using Microsoft::WRL::ComPtr;
 // Global variables
 ComPtr<ID3D11Device> g_d3dDevice;
 ComPtr<ID3D11DeviceContext> g_d3dContext;
-ComPtr<IDXGISwapChain1> g_swapChain;
+ComPtr<IDXGISwapChain3> g_swapChain;
 ComPtr<ID2D1Factory1> g_d2dFactory;
 ComPtr<ID2D1Device> g_d2dDevice;
 ComPtr<ID2D1DeviceContext> g_d2dContext;
 ComPtr<ID2D1Bitmap1> g_d2dTargetBitmap;
 ComPtr<ID2D1SolidColorBrush> g_whiteBrush;
+ComPtr<ID2D1SolidColorBrush> g_innerBrush;
 
 HWND g_hwnd = nullptr;
+float g_innerBrightness = 400.0f; // nits
 int g_screenWidth = 0;
 int g_screenHeight = 0;
 
@@ -158,7 +160,7 @@ bool InitD3D()
     DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
     swapChainDesc.Width = g_screenWidth;
     swapChainDesc.Height = g_screenHeight;
-    swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+    swapChainDesc.Format = DXGI_FORMAT_R16G16B16A16_FLOAT;
     swapChainDesc.SampleDesc.Count = 1;
     swapChainDesc.SampleDesc.Quality = 0;
     swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
@@ -166,14 +168,26 @@ bool InitD3D()
     swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
     swapChainDesc.Flags = 0;
 
+    ComPtr<IDXGISwapChain1> swapChain1;
     hr = dxgiFactory->CreateSwapChainForHwnd(
         g_d3dDevice.Get(),
         g_hwnd,
         &swapChainDesc,
         nullptr,
         nullptr,
-        &g_swapChain
+        &swapChain1
     );
+
+    if (FAILED(hr))
+        return false;
+
+    // Get IDXGISwapChain3 for color space setting
+    hr = swapChain1.As(&g_swapChain);
+    if (FAILED(hr))
+        return false;
+
+    // Set scRGB color space
+    hr = g_swapChain->SetColorSpace1(DXGI_COLOR_SPACE_RGB_FULL_G10_NONE_P709);
 
     return SUCCEEDED(hr);
 }
@@ -221,7 +235,7 @@ bool InitD2D()
         return false;
 
     D2D1_BITMAP_PROPERTIES1 bitmapProperties = {};
-    bitmapProperties.pixelFormat.format = DXGI_FORMAT_B8G8R8A8_UNORM;
+    bitmapProperties.pixelFormat.format = DXGI_FORMAT_R16G16B16A16_FLOAT;
     bitmapProperties.pixelFormat.alphaMode = D2D1_ALPHA_MODE_PREMULTIPLIED;
     bitmapProperties.bitmapOptions = D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW;
 
@@ -236,10 +250,20 @@ bool InitD2D()
 
     g_d2dContext->SetTarget(g_d2dTargetBitmap.Get());
 
-    // Create white brush
+    // Create white brush at 10000 nits (10000/80 = 125.0 in scRGB)
     hr = g_d2dContext->CreateSolidColorBrush(
-        D2D1::ColorF(D2D1::ColorF::White),
+        D2D1::ColorF(125.0f, 125.0f, 125.0f, 1.0f),
         &g_whiteBrush
+    );
+
+    if (FAILED(hr))
+        return false;
+
+    // Create inner brush with variable brightness
+    float innerScRGB = g_innerBrightness / 80.0f;
+    hr = g_d2dContext->CreateSolidColorBrush(
+        D2D1::ColorF(innerScRGB, innerScRGB, innerScRGB, 1.0f),
+        &g_innerBrush
     );
 
     return SUCCEEDED(hr);
@@ -261,6 +285,15 @@ void Render()
     D2D1_RECT_F rect = D2D1::RectF(x, y, x + rectWidth, y + rectHeight);
     g_d2dContext->FillRectangle(&rect, g_whiteBrush.Get());
 
+    // Draw inner rectangle (1/3 size) centered in the outer rectangle
+    float innerWidth = rectWidth / 3.0f;
+    float innerHeight = rectHeight / 3.0f;
+    float innerX = x + (rectWidth - innerWidth) / 2.0f;
+    float innerY = y + (rectHeight - innerHeight) / 2.0f;
+
+    D2D1_RECT_F innerRect = D2D1::RectF(innerX, innerY, innerX + innerWidth, innerY + innerHeight);
+    g_d2dContext->FillRectangle(&innerRect, g_innerBrush.Get());
+
     g_d2dContext->EndDraw();
 
     // Present
@@ -269,6 +302,7 @@ void Render()
 
 void CleanUp()
 {
+    g_innerBrush.Reset();
     g_whiteBrush.Reset();
     g_d2dTargetBitmap.Reset();
     g_d2dContext.Reset();
